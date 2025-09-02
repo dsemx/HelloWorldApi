7a68ba06fd17b920f560f7a49088bd3a22d2f3a5
Josue David Segovia Carrizales <jdavid.segovia@gmail.com>
2025-09-01 23:22:02 -0500

    First commit for Hello World API

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void HelloWorld_Test()
+    {
+        var message = "Hello, World!";
+        Assert.Equal("Hello, World!", message);
+    }
+}
